ce2d9c14fc8e735ce0cb76d1790cd2d5f06f055c
Flávia Reis <flaviarster@gmail.com>
2024-02-08 05:51:55 -0300

    Adjusting

--- src/components/ShareComponent.jsx
+++ src/components/ShareComponent.jsx
@@ -15,7 +15,7 @@ export const ShareComponent = ({ setShowShare, setShowAvatar }) => {
     <div className='lg:relative'>
         <div  onMouseLeave={handleMouseLeave} className='flex justify-between items-center py-6 px-8 bg-veryDarkGrayishBlue lg:justify-center lg:h-63 lg:w-248 lg:rounded-xl lg:absolute lg:right-0 lg:translate-x-20 lg:bottom-20 lg:z-50'>
             <div className='lg:w-5 lg:h-5 lg:absolute lg:top-12 lg:bg-veryDarkGrayishBlue lg:rotate-45 lg:z-10'/>
-            <div className='flex justify-start items-center gap-3'>
+            <div className='absolute flex justify-start items-center gap-4'>
             <p className='tracking-widest text-13 text-desaturatedDarkBlue'>SHARE</p>
             <a href="#"><img src={ facebook } alt="Facebook Icon" /></a>
             <a href="#"><img src={ twitter } alt="Twitter Icon" /></a>
